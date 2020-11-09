f01e3c27b7e7b32866f6d4fd5da8c448116d040c
hunghnuit <hunghn0999@gmail.com>
2020-11-09 13:17:14 +0700

    Bo sung swiper cho storage, chinh sua mot it o js va css cua storage vi xung dot voi swiper

--- Assets/js/Storage.js
+++ Assets/js/Storage.js
@@ -1,15 +1,44 @@
 $(document).ready(function () {
     // DECLARE INITIAL ACTION (LIKE HIDING SOME TABS, LOADING SOMETHING)
-    $(".storage-tab").hide(); // ẩn tất cả các tab ở storage, chỉ hiện tab mặc định là picture-storage-container.
+    
+    // ẩn tất cả các tab ở storage, chỉ hiện tab mặc định là picture-storage-container.
+    //không thể dùng .hide() vì swiper không cho => phải dùng important
+    $(".storage-tab").attr( "style", "display: none !important;" ); 
     $("#picture-storage-container").show();
+    
+    // For swiper
+    new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+
+        // Prevent not changing width anymore
+        slidesPerView:'auto'
+    })
 
 
     // DECLARE HELPER FUNCTION
     function showStorageTab(clickedBtn){
         let idClickedBtn = clickedBtn.attr('id');
         
-        // Ẩn tất cả các tab.
-        $(".storage-tab").hide();
+        // Ẩn tất cả các tab, không thể dùng .hide() vì bị swiper không cho => phải dùng important
+        $(".storage-tab").attr( "style", "display: none !important;" )
 
         // Hiện mỗi tab click
         if(idClickedBtn === 'profile-customized-btn'){
