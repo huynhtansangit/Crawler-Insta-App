c2654b452057609448fbc20d94835a466e6b796c
hunghnuit <hunghn0999@gmail.com>
2020-11-13 17:35:19 +0700

    Them phan edit picture, chua xu ly xong event click, doi ten mot so cho cho hop ly

--- Assets/js/EditPicture.js
+++ Assets/js/EditPicture.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     // DECLARE INITIAL ACTION (LIKE HIDING SOME TABS, LOADING SOMETHING)
 
+    // ẩn tất cả các edit-pic-option.
+    $(".edit-pic-option").hide();
+    $("#edit-pic-text-option").show();
 
 
     // DECLARE HELPER FUNCTION
@@ -8,5 +11,13 @@ $(document).ready(function () {
 
 
     // DECLARE EVENT HANDLER
+    $(".edit-pic-main-option").on("click", function(){
+        $(".edit-pic-main-option").removeClass("selected-edit-pic-main-option"); //bỏ lựa chọn cái nút đã select trước đó.
+        $(this).addClass("selected-edit-pic-main-option"); //select nút mới.
 
+        // Ẩn các cái menu còn lại.
+        // $("").hide();
+        // show cái mới được chọn.
+        
+    });
 })
